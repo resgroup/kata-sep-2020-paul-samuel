0d0db301f9920d56057f8faba76f17f765e4c89c
PaulBDavies <paul.davies@res-ltd.com>
2020-09-29 10:09:22 +0100

    Name parameters

--- Test.py
+++ Test.py
@@ -2,27 +2,27 @@ import Tennis
 
 
 def test_first_point():
-    Tennis.set_score(True, 0)
-    Tennis.set_score(False, 0)
+    Tennis.set_score(is_server=True, score=0)
+    Tennis.set_score(is_server=False, score=0)
 
-    Tennis.score_point(True)
+    Tennis.score_point(is_server=True)
 
     assert Tennis.get_score() == "15:0"
 
 
 def test_returner_point():
-    Tennis.set_score(True, 15)
-    Tennis.set_score(False, 15)
+    Tennis.set_score(is_server=True, score=15)
+    Tennis.set_score(is_server=False, score=15)
 
-    Tennis.score_point(False)
+    Tennis.score_point(is_server=False)
 
     assert Tennis.get_score() == "15:30"
 
 
 def test_server_point():
-    Tennis.set_score(True, 30)
-    Tennis.set_score(False, 30)
+    Tennis.set_score(is_server=True, score=30)
+    Tennis.set_score(is_server=False, score=30)
 
-    Tennis.score_point(True)
+    Tennis.score_point(is_server=True)
 
     assert Tennis.get_score() == "40:30"
