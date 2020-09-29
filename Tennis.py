6c1a8a5a27901ac9360008aefdbe6f3962edf471
Samuel Naylor <50234141+samuelwnaylor@users.noreply.github.com>
2020-09-29 10:44:37 +0100

    Create class for Tennis Game

--- Tennis.py
+++ Tennis.py
@@ -1,19 +1,48 @@
-server_score = 0
-returner_score = 0
 
+class TennisGame:
 
-def score_point(is_server):
-    return
+    def __init__(self):
+        self.server_score = 0
+        self.returner_score = 0
 
+    def score_point(self, is_server):
+        if is_server:
+            self.server_score = self.add_to_existing_score(self.server_score, opponent_score=self.returner_score)
+        else:
+            self.returner_score = self.add_to_existing_score(self.returner_score, opponent_score=self.server_score)
 
-def set_score(is_server, score):
-    if is_server:
-        server_score = score
-    else:
-        returner_score = score
+    @staticmethod
+    def add_to_existing_score(score, opponent_score):
+        if score == 0:
+            return 15
+        elif score == 15:
+            return 30
+        elif score == 30:
+            return 40
+        elif score == 40:
+            if opponent_score == 40:
+                return 'A'
+            elif opponent_score == 'A':
+                # ToDo: Set self.returner_score to 40 using self.set_score() perhaps
+                pass
+            else:
+                return 50
+        elif score == 'A':
+            return 50
+        else:
+            raise ValueError('Score is already maximum 40')
 
-    return
+    def set_score(self, is_server, score):
+        if is_server:
+            self.server_score = score
+        else:
+            self.returner_score = score
 
+    def get_score(self):
+        return str(self.server_score) + ":" + str(self.returner_score)
 
-def get_score():
-    return str(server_score) + ":" + str(returner_score)
+    def server_wins(self):
+        return self.server_score == 50
+
+    def returner_wins(self):
+        return self.returner_score == 50
--- Test.py
+++ Test.py
@@ -1,28 +1,99 @@
-import Tennis
+from Tennis import TennisGame
 
 
 def test_first_point():
-    Tennis.set_score(True, 0)
-    Tennis.set_score(False, 0)
+    tennis_game = TennisGame()
+    tennis_game.set_score(True, 0)
+    tennis_game.set_score(False, 0)
 
-    Tennis.score_point(True)
+    tennis_game.score_point(True)
 
-    assert Tennis.get_score() == "15:0"
+    assert tennis_game.get_score() == "15:0"
 
 
 def test_returner_point():
-    Tennis.set_score(True, 15)
-    Tennis.set_score(False, 15)
+    tennis_game = TennisGame()
+    tennis_game.set_score(True, 15)
+    tennis_game.set_score(False, 15)
 
-    Tennis.score_point(False)
+    tennis_game.score_point(False)
 
-    assert Tennis.get_score() == "15:30"
+    assert tennis_game.get_score() == "15:30"
 
 
 def test_server_point():
-    Tennis.set_score(True, 30)
-    Tennis.set_score(False, 30)
+    tennis_game = TennisGame()
+    tennis_game.set_score(True, 30)
+    tennis_game.set_score(False, 30)
 
-    Tennis.score_point(True)
+    tennis_game.score_point(True)
 
-    assert Tennis.get_score() == "40:30"
+    assert tennis_game.get_score() == "40:30"
+
+
+def test_server_wins():
+    tennis_game = TennisGame()
+    tennis_game.set_score(True, 40)
+    tennis_game.set_score(False, 30)
+
+    tennis_game.score_point(True)
+
+    assert tennis_game.server_wins()
+
+
+def test_returner_wins():
+    tennis_game = TennisGame()
+    tennis_game.set_score(True, 30)
+    tennis_game.set_score(False, 40)
+
+    tennis_game.score_point(False)
+
+    assert tennis_game.returner_wins()
+
+
+def test_server_advantage():
+    tennis_game = TennisGame()
+    tennis_game.set_score(True, 40)
+    tennis_game.set_score(False, 40)
+
+    tennis_game.score_point(True)
+
+    assert not tennis_game.server_wins()
+
+    assert tennis_game.get_score() == "A:40"
+
+
+def test_returner_advantage():
+    tennis_game = TennisGame()
+    tennis_game.set_score(True, 40)
+    tennis_game.set_score(False, 40)
+
+    tennis_game.score_point(False)
+
+    assert not tennis_game.returner_wins()
+
+    assert tennis_game.get_score() == "40:A"
+
+
+def test_server_deuce():
+    tennis_game = TennisGame()
+    tennis_game.set_score(True, 40)
+    tennis_game.set_score(False, 'A')
+
+    tennis_game.score_point(True)
+
+    assert not tennis_game.server_wins()
+
+    assert tennis_game.get_score() == "40:40"
+
+
+def test_returner_deuce():
+    tennis_game = TennisGame()
+    tennis_game.set_score(True, 'A')
+    tennis_game.set_score(False, 40)
+
+    tennis_game.score_point(False)
+
+    assert not tennis_game.returner_wins()
+
+    assert tennis_game.get_score() == "40:40"
