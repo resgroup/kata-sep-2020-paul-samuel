6aaf011b776e291a712deb334f07ebdace6be0e7
PaulBDavies <paul.davies@res-ltd.com>
2020-09-29 11:54:09 +0100

    Turn scores into dictionary

--- Tennis.py
+++ Tennis.py
@@ -18,14 +18,21 @@ class TennisPlayer(Enum):
 class TennisGame:
 
     def __init__(self, server_score, receiver_score):
-        self._server_score = server_score
-        self._returner_score = receiver_score
+        self._scores = { TennisPlayer.Server: server_score, TennisPlayer.Receiver: receiver_score }
 
     def score_point(self, player):
         if player == TennisPlayer.Server:
-            self._server_score, self._returner_score = self._add_to_existing_score(self._server_score, opponent_score=self._returner_score)
+            self._scores[TennisPlayer.Server], self._scores[TennisPlayer.Receiver] = \
+                self._add_to_existing_score(
+                    self._scores[TennisPlayer.Server],
+                    opponent_score=self._scores[TennisPlayer.Receiver]
+                )
         else:
-            self._returner_score, self._server_score = self._add_to_existing_score(self._returner_score, opponent_score=self._server_score)
+            self._scores[TennisPlayer.Receiver], self._scores[TennisPlayer.Server] = \
+                self._add_to_existing_score(
+                    self._scores[TennisPlayer.Receiver],
+                    opponent_score=self._scores[TennisPlayer.Server]
+                )
 
     @staticmethod
     def _add_to_existing_score(score, opponent_score):
@@ -48,10 +55,7 @@ class TennisGame:
             raise ValueError('Invalid score: ' + str(score))
 
     def get_score(self):
-        return self._server_score.value + ":" + self._returner_score.value
+        return self._scores[TennisPlayer.Server].value + ":" + self._scores[TennisPlayer.Receiver].value
 
     def is_winner(self, player):
-        if player == TennisPlayer.Server:
-            return self._server_score == TennisScore.Win
-        else:
-            return self._returner_score == TennisScore.Win
+        return self._scores[player] == TennisScore.Win
