86a64b262839dcab048f96ff6cb3e482f4e587da
PaulBDavies <paul.davies@res-ltd.com>
2020-09-29 09:49:42 +0100

    First breakout

--- Tennis.py
+++ Tennis.py
@@ -1,4 +1,19 @@
-def dummy_method(input):
-    return "output"
+server_score = 0
+returner_score = 0
 
 
+def score_point(is_server):
+    return
+
+
+def set_score(is_server, score):
+    if is_server:
+        server_score = score
+    else:
+        returner_score = score
+
+    return
+
+
+def get_score():
+    return str(server_score) + ":" + str(returner_score)
